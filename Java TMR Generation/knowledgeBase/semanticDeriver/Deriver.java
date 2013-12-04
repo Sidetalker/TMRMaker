0013bd226a4f2721ecb5b8f0792ad1b8c2010ad8
dwight naylor <dwight.naylor@sbcglobal.net>
2013-12-04 15:56:44 -0500

    "thank you" support

--- Java TMR Generation/knowledgeBase/semanticDeriver/Deriver.java
+++ Java TMR Generation/knowledgeBase/semanticDeriver/Deriver.java
@@ -509,7 +509,7 @@ public class Deriver {
 		// String sentence = "When is Joe's Pizza open?";
 		// String sentence = "I want to find a nice place to eat sometime.";
 		// String sentence = "I like Mexican.";
-		 String sentence = "I want to find a nice place for a dinner with my father tomorrow at 7 pm.";
+		 String sentence = "goodbye.";
 		// String sentence =
 		// "Could you give me a place I could eat at sometime?";
 //		String sentence = "Does Joe's Pizza take Visa?";
